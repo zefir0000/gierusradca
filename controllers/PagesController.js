b8b7dd8c35105e6454760d0d617731f9e9d04035
slawek <>
2024-03-23 11:23:40 +0100

    policy and ranki

--- controllers/PagesController.js
+++ controllers/PagesController.js
@@ -3,8 +3,11 @@ exports.home = async (req, res) => {
 
   res.render('index');
 };
+exports.policy = async (req, res) => {
+
+  res.render('policy');
+};
 exports.blog = async (req, res) => {
   console.log(req.params)
   res.render('blog/'+req.params.slug.replace('.html', ''));
 };
-
--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,6 @@ const PagesController = require('../controllers/PagesController');
 
 router.get('/', PagesController.home);
 router.get('/blog/:slug', PagesController.blog);
+router.get('/policy/', PagesController.policy);
 
 module.exports = router;
