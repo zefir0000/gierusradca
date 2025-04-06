8a81ca92575fc4c97e6fb1c8365d0b66ffb8789d
Zefir0000 <slawek.lenczner@gmail.com>
2025-04-06 10:23:36 +0200

    add image

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const admin = express.Router();
 const multer = require("multer");
-const path = require("path");
 const auth = require('../middlewares/basicAuth');
 
 const AdminController = require('../controllers/AdminController');
@@ -9,7 +8,7 @@ const AdminController = require('../controllers/AdminController');
 const storage = multer.diskStorage({
   destination: "public/images/",
   filename: (req, file, cb) => {
-      cb(null, Date.now() + path.extname(file.originalname));
+      cb(null, file.originalname);
   }
 });
 const upload = multer({ storage });
@@ -21,6 +20,8 @@ admin.get('/edit-page/:slug', AdminController.editPage);
 admin.post('/update/', auth, AdminController.update);
 admin.post('/page/', upload.single("image"), AdminController.addPage);
 admin.post('/update-page', auth, AdminController.updatePage);
+admin.get('/add-image', AdminController.uploadImage);
 
+admin.post('/page/image', upload.single("image"), AdminController.addImage);
 
 module.exports = admin;
