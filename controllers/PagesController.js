fb585645e6b61f652a5ad2e7962f967287ca5800
Zefir0000 <slawek.lenczner@gmail.com>
2025-05-29 14:01:47 +0200

    fix images and publish date function

--- controllers/PagesController.js
+++ controllers/PagesController.js
@@ -2,16 +2,19 @@ const fs = require("fs");
 
 exports.home = async (req, res) => {
   const data = await fs.readFileSync('common/blogList.txt', { encoding: 'utf8' });
-  const blogList = JSON.parse(data).filter(x => x.active);
+  console.log()
+  const blogList = JSON.parse(data).filter(x => x.active).filter(d => new Date(d.publishDate) < new Date());
+
   res.render('index', { blogList});
 };
 exports.policy = async (req, res) => {
-
+  const data = await fs.readFileSync('common/blogList.txt', { encoding: 'utf8' });
+  const blogList = JSON.parse(data).filter(x => x.active).filter(d => new Date(d.publishDate) < new Date());;
   res.render('policy', { blogList});
 };
 exports.blog = async (req, res) => {
   const data = await fs.readFileSync('common/blogList.txt', { encoding: 'utf8' });
-  const blogList = JSON.parse(data).filter(x => x.active);
+  const blogList = JSON.parse(data).filter(x => x.active).filter(d => new Date(d.publishDate) < new Date());;
   console.log(req.params)
   res.render('blog/'+req.params.slug.replace('.html', ''), { blogList});
 };
