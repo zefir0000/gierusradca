6e11247d738fa94f7dd08a09bf37d9f37b040ab8
zefir0000 <zefir0000@o2.pl>
2025-06-07 07:19:51 +0200

    from server

--- middlewares/redirect.js
+++ middlewares/redirect.js
@@ -2,12 +2,14 @@ module.exports = function (req, res, next) {
   const host = req.headers.host
   const url = req.url
   const forceDomain = 'https://www.moto-trade.pl'
-
-  if ( host.match(/moto-trade/)) {
+console.log(host, url, req);
+  if ( host.match(/motqweo-trade/)) {
 
     res.writeHead(301, { Location: forceDomain + url })
     return res.end()
   }
-
+  if (host.match(/www.radcagierus/)){
+	      res.writeHead(301, { Location: 'https://gierusradca.pl' + url })
+  }
   return next()
 }
