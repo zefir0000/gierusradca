3f79db8648c26fce37ea4d0626c7eb72ebb551cb
Zefir0000 <slawek.lenczner@gmail.com>
2025-06-14 10:15:18 +0200

    remove console

--- middlewares/redirect.js
+++ middlewares/redirect.js
@@ -2,7 +2,6 @@ module.exports = function (req, res, next) {
   const host = req.headers.host
   const url = req.url
   const forceDomain = 'https://www.moto-trade.pl'
-console.log(host, url, req);
   if ( host.match(/motqweo-trade/)) {
 
     res.writeHead(301, { Location: forceDomain + url })
