9c7a021ea662f92cfd2b03e1c7a0e6fd4ad5df57
Zefir0000 <slawek.lenczner@gmail.com>
2025-06-15 15:32:56 +0200

    fixed blog

--- controllers/PagesController.js
+++ controllers/PagesController.js
@@ -20,7 +20,11 @@ exports.blog = async (req, res) => {
   const blogList = JSON.parse(data).filter(x => x.active).filter(d => new Date(d.publishDate) < new Date());
   console.log(req.params)
   const post = blogList.find(post => post.slug === req.params.slug);
-  post.publishDate = helper.formatDate(post.publishDate || '2025-06-16');
+  if (post) {
+    post.publishDate = helper.formatDate(post.publishDate);
+  } else {
+    post.publishDate = ''
+  }
   res.render('blog/'+req.params.slug.replace('.html', ''), { blogList, post });
 };
 
